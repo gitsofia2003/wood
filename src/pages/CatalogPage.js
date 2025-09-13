a73d1ddffc13abb23224d652df78d0685b406dba
rasuljan507 <ryusufaliyev@gmail.com>
2025-09-13 17:24:30 +0700

    added colorfilter size

--- src/pages/CatalogPage.js
+++ src/pages/CatalogPage.js
@@ -59,7 +59,7 @@ const CatalogPage = () => {
         <main className="container mx-auto px-6 py-12">
             <h1 className="text-3xl md:text-4xl font-bold text-gray-800 text-center mb-12">Каталог товаров</h1>
             
-            <div className="mb-6">
+            <div className="mb-3">
                 <CategoryFilter
                     activeCategory={activeCategory}
                     setActiveCategory={setActiveCategory}
